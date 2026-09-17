e9237c745fa2cfeba10db48d299eeec3d3751012
valvalentine11-dot <valvalentine11@gmail.com>
2026-09-17 19:31:05 +0100

    Update Follow.js

--- src/models/Follow.js
+++ src/models/Follow.js
@@ -8,7 +8,7 @@ const followSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Prevent duplicate follow relationships
+
 followSchema.index({ follower: 1, following: 1 }, { unique: true });
 
 module.exports = mongoose.model('Follow', followSchema);
